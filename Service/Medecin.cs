c10132703384c83355d547b43bdd443b0433caba
Mikael Heyraud <mikaelhey@hotmail.fr>
2025-09-04 11:07:50 +0200

    Observer medecin et secretaire + debut singleton Hospital

---
+++ Service/Medecin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    public class Medecin : IObserverMedecin
+    {
+        public string Nom { get; }
+        public int Salle { get; }
+
+        public Medecin(string nom, int salle)
+        {
+            Nom = nom;
+            Salle = salle;
+        }
+
+        public void NotifierNouveauPatient(Patient.Patient patient)
+        {
+            if (patient == null)
+                Console.WriteLine($"[Médecin {Nom} - Salle {Salle}] Aucun patient en attente.");
+            else
+                Console.WriteLine($"[Médecin {Nom} - Salle {Salle}] Prochain patient : {patient.Nom} {patient.Prenom}");
+        }
+    }
+}
