a907fda1e215f44831d38e2195304b4330b46b59
Mezier <50947908+TheD3us@users.noreply.github.com>
2025-09-04 10:10:35 +0200

    Ajout Db

---
+++ ProjetHopital/Db/Db.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DllVisites;
+
+
+
+namespace ProjetHopital.Db
+{
+    public partial class Db : Component
+    {
+        public Db()
+        {
+            InitializeComponent();
+        }
+
+        public Db(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+
+        //Partie Visite
+        public List<Visites> selectAllVisites()
+        {
+            List<Visites> ListeVisite = new List<Visites>();
+            var reader = sqlCmd.ExecuteReader();
+
+            sqlCo.Open();
+            sqlCmd.CommandText = "select idpatient, date, medecin, num-salle, tarif "+
+                                 "  from visite                                     ";
+
+            while(reader.Read())
+            {
+                Visites v = new Visites(Convert.ToInt32(reader["idpatient"].ToString()), 
+                                        Convert.ToDateTime(reader["date"].ToString()), 
+                                        Convert.ToInt32(reader["medecin"].ToString()),
+                                        Convert.ToInt32(reader["num-salle"].ToString()),
+                                        Convert.ToDouble(reader["tarif"].ToString())
+                                        );
+                ListeVisite.Add(v);
+            }
+
+            sqlCo.Close();
+            return ListeVisite;
+        }    
+        
+
+        //Partie Patient
+
+
+
+        //Partie Authentification
+
+
+    }
+
+    
+}
